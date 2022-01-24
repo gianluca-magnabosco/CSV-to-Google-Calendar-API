0b26c0502e1f5a19f76418bbdb0c9124d78dcd23
gianluca-magnabosco <gianluca_1406@hotmail.com>
2022-01-24 18:48:48 -0300

    several changes and optimization

--- CSV-to-Google-Calendar-API.py
+++ CSV-to-Google-Calendar-API.py
@@ -59,7 +59,7 @@ def convert_to_RFC_datetime(start_year=1900, start_month=1, start_day=1, hour=0,
 
 
 # launch api
-CLIENT_SECRET_FILE = "client_secret.json"
+CLIENT_SECRET_FILE = "client_secret.json"      ################ do not forget to download OAuth json file and put it in current directory!!!!!!!!!!
 API_NAME = 'calendar'
 API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -142,7 +142,7 @@ def insert_events(color):
             x += 1
 
         else:
-            adjust_timezone = 3 # (this is for UTC-3)         ### CHANGE TO YOUR OWN TIMEZONE, COULD BE -n, +n, BASED ON UTC TIME (0)
+            adjust_timezone = 3 # (this is for UTC-3)         ### CHANGE TO YOUR OWN TIMEZONE, COULD BE -n, +n, or none, BASED ON UTC TIME (0)
             event_request_body = {
             'start':{
                 'dateTime': convert_to_RFC_datetime(int(start_year[i]), int(start_month[i]), int(start_day[i]), int(fstart_time[i]) + adjust_timezone, 0),
@@ -183,13 +183,16 @@ wb = load_workbook('excel_file.xlsx')
 ws = wb.active
 
 
+# delete header row
+ws.delete_rows(1)
+
 # row number variable
-max_rows = ws.max_row-1
+max_rows = ws.max_row
 
 
 # copy subject
 subject = []
-for i in range(2,max_rows+2):
+for i in range(1,max_rows+1):
     subject.append(ws.cell(row = i, column = 1).value)
 
 
@@ -197,26 +200,22 @@ for i in range(2,max_rows+2):
 ## FORMATTING START DATE
 # copy start date
 start_date = []
-for i in range(1,max_rows+2):
+for i in range(1,max_rows+1):
     start_date.append(ws.cell(row = i, column = 2).value)
 
-# format start month
+# append lists
 start_month = []
-for i in range(1,max_rows+1):
-    start_month.append(start_date[i])
-start_month = [x[:-6] for x in start_month]
-
-# format start day
 start_day = []
-for i in range(1,max_rows+1):
-    start_day.append(start_date[i])
-start_day = [x[3:-3] for x in start_day]
-
-# format start year
 start_year = []
-for i in range(1,max_rows+1):
+
+# format start month, day & year
+for i in range(0,max_rows):
+    start_month.append(start_date[i])
+    start_day.append(start_date[i])
     start_year.append(start_date[i])
-    print(start_year)
+
+start_month = [x[:-6] for x in start_month]
+start_day = [x[3:-3] for x in start_day]
 start_year = ['20' + x[6:] for x in start_year]
 
 
@@ -224,12 +223,12 @@ start_year = ['20' + x[6:] for x in start_year]
 ## FORMATTING START TIME
 # copy start time
 start_time = []
-for i in range(1,max_rows+2):
+for i in range(1,max_rows+1):
     start_time.append(ws.cell(row = i, column = 3).value)
 
 # format start time
 fstart_time = []
-for i in range(1,max_rows+1):
+for i in range(0,max_rows):
     fstart_time.append(start_time[i])
 fstart_time = [x[:-3] for x in fstart_time]
 
@@ -239,47 +238,48 @@ fstart_time = [x[:-3] for x in fstart_time]
 ## FORMATTING END DATE
 # copy end date
 end_date = []
-for i in range(1,max_rows+2):
+for i in range(1,max_rows+1):
     end_date.append(ws.cell(row = i, column = 4).value)
 
-# format end month
+# append lists
 end_month = []
-for i in range(1,max_rows+1):
-    end_month.append(end_date[i])
-end_month = [x[:-6] for x in end_month]
-
-# format end day
 end_day = []
-for i in range(1,max_rows+1):
-    end_day.append(end_date[i])
-end_day = [x[3:-3] for x in end_day]
-
-# format end year
 end_year = []
-for i in range(1,max_rows+1):
+
+# format end month, day & year
+for i in range(0,max_rows):
+    end_month.append(end_date[i])
+    end_day.append(end_date[i])
     end_year.append(end_date[i])
+
+end_month = [x[:-6] for x in end_month]
+end_day = [x[3:-3] for x in end_day]
 end_year = ['20' + x[6:] for x in end_year]
+
 
 
 
 ## FORMATTING END TIME
 # copy end time
 end_time = []
-for i in range(1,max_rows+2):
+for i in range(1,max_rows+1):
     end_time.append(ws.cell(row = i, column = 5).value)
 
 # format end time
 fend_time = []
-for i in range(1,max_rows+1):
+for i in range(0,max_rows):
     fend_time.append(end_time[i])
 fend_time = [x[:-3] for x in fend_time]
 
 
 
+
 # copy all-day event 
 all_day_event = []
-for i in range(2,max_rows+2):
+for i in range(1,max_rows+1):
     all_day_event.append(ws.cell(row = i, column = 6).value)
+
+# padronize all possible letter case options (by uppercasing them all)
 for i in range(0,max_rows):
     all_day_event[i] = all_day_event[i].upper()
 
@@ -287,22 +287,24 @@ for i in range(0,max_rows):
 
 # copy description
 description = []
-for i in range(2,max_rows+2):
+for i in range(1,max_rows+1):
     description.append(ws.cell(row = i, column = 7).value)
 
 
 
 # copy location
 location = []
-for i in range(2,max_rows+2):
+for i in range(1,max_rows+1):
     location.append(ws.cell(row = i, column = 8).value)
 
 
 
 # copy private
 is_private = []
-for i in range(2,max_rows+2):
+for i in range(1,max_rows+1):
     is_private.append(ws.cell(row = i, column = 9).value)
+
+# adapt private to fit json
 for i in range(0,max_rows):
     is_private[i] = is_private[i].upper()
     if "TRUE" in is_private[i]:
